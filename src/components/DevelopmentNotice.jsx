9359805682be960f30614ab142bf300b579291ef
theone11show <team@theone11.show>
2025-08-03 11:08:14 +0530

    Update DevelopmentNotice.jsx
    Removed the DevelopmentNotice

--- src/components/DevelopmentNotice.jsx
+++ src/components/DevelopmentNotice.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 
 const DevelopmentNotice = () => {
   return (
@@ -59,4 +59,4 @@ const DevelopmentNotice = () => {
   );
 };
 
-export default DevelopmentNotice;
+export default DevelopmentNotice;*/
