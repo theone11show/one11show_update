b08cf492108cb509f80e6fc0ced7107d6910363a
Vikramjeetblog <horavikram21@gmail.com>
2025-08-09 18:55:28 +0530

    Poster updated in update page

--- src/components/Updates.jsx
+++ src/components/Updates.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import UpdatesSection from './UpdatesSection';
 import CompetitionCards from './CompetitionCards';
+import UpcomingEvents from './UpcomingEvents'
  
 function updates() {
   return (
     <div>
       <UpdatesSection/>
+      <UpcomingEvents/>
       <CompetitionCards/>
     </div>
   );
