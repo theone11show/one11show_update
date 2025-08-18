877854cb66dcaee5a7eb2399021541ba30f10163
theone11show <team@theone11.show>
2025-08-18 16:53:26 +0530

    Update AboutSection.js

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -13,7 +13,7 @@ const AboutSection = () => {
     <section className="relative h-screen flex items-center justify-center text-center overflow-hidden pt-16">
       {/* Background Video */}
       <video autoPlay loop muted playsInline className="absolute top-0 left-0 w-full h-full object-cover z-0">
-        <source src="https://xwbiqeigjdpuzfqw.public.blob.vercel-storage.com/cactus%20%282%29.mp4" type="video/mp4" />
+        <source src="https://xwbiqeigjdpuzfqw.public.blob.vercel-storage.com/Website%20faktus.mov" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
