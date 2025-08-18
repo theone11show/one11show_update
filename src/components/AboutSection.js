5532f3acf8743ae8dc68c4a363f424ac618fa8aa
theone11show <team@theone11.show>
2025-08-18 17:28:37 +0530

    Update AboutSection.js

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,5 @@
-import { Link } from "react-router-dom";
-
-import Logo from "../assets/HeroSection.jpeg";
+import { Link } from "react-router-dom;
+  import Logo from "../assets/HeroSection.jpeg;
 
 const AboutSection = () => {
   const handleScroll = (e) => {
