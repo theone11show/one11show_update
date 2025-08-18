1d9cf0e1b70e04e971c1c7612715baa7ee7a6ec9
theone11show <team@theone11.show>
2025-08-18 11:43:20 +0530

    Update UpcomingEvents.js

--- src/components/UpcomingEvents.js
+++ src/components/UpcomingEvents.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Cactus from "../assets/CACTUS.jpg";
-import FAKIRA from "../assests/FAKIRA.JPG:
+import FAKIRA from "../assests/FAKIRA.jpg:
 
 export default function UpcomingEvent() {
   const navigate = useNavigate();
