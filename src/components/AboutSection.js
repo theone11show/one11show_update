45f57240b7ff7c2712a42ac9cbb66125f711af62
theone11show <team@theone11.show>
2025-08-18 19:59:40 +0530

    Update AboutSection.js

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from '../assets/one11show.png";
+import logo from "../assets/one11show.png";
 
 const AboutSection = () => {
   const handleScroll = (e) => {
