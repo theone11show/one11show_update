15472c5c979fd61eaa7bc39c77bfd70d9a60c972
theone11show <team@theone11.show>
2025-08-20 13:46:15 +0530

    Update AboutSection.js
    updated logo

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../assets/logo2.png";
+import Logo from "../assets/one11showlogo";
 
 const AboutSection = () => {
   const handleScroll = (e) => {
