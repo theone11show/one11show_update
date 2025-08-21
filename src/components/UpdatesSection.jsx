93b6bf8360902dd734f67304b00f691dc49565bf
theone11show <team@theone11.show>
2025-08-21 16:56:26 +0530

    Update UpdatesSection.jsx
    update the gap between the Cards

--- src/components/UpdatesSection.jsx
+++ src/components/UpdatesSection.jsx
@@ -68,13 +68,7 @@ const LatestUpdates = () => {
           Latest Updates
         </h2>
 
-        <div className="flex justify-center mb-14">
-          <img
-            src={updateImage}
-            alt="Announcements"
-            className="w-full max-w-md h-[180px] object-cover rounded-lg"
-          />
-        </div>
+        
 
         {/* Timeline */}
         <div className="relative">
@@ -82,7 +76,7 @@ const LatestUpdates = () => {
           <div className="absolute left-1/2 -translate-x-1/2 w-[3px] h-full bg-cyan-400 shadow-[0_0_22px_3px_rgba(0,255,255,0.6)]"></div>
 
           {/* Cards */}
-          <div className="space-y-4 sm:space-y-6 md:space-y-8"">
+          <div className="space-y-4 sm:space-y-6 md:space-y-8">
             {updates.map((item, i) => {
               const isLeft = i % 2 === 0;
               return (
