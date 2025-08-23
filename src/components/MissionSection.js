36bcbc60040e84c3a95b5ccfc5d51d478a40f43d
theone11show <team@theone11.show>
2025-08-23 18:12:39 +0530

    Update MissionSection.js
    updated the mission

--- src/components/MissionSection.js
+++ src/components/MissionSection.js
@@ -3,15 +3,17 @@ import React from "react";
 const MissionSection = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-gray-200 text-center p-10">
-      <p className="mb-6 text-lg text-gray-700">
-        We are a unified group of industry music professionals, including
-        producers, directors, artists, performers, and experienced event
-        managers.
-        <br /><br />
-        Our shared mission is to provide an unbiased platform for raw talents.
-      </p>
-      <button className="bg-orange-500 hover:bg-orange-600 text-white px-6 py-2 rounded-full">
-        connect
+      <div className="max-w-3xl"> 
+        <p className="mb-6 text-small text-gray-700 leading-relaxed">
+          We are a unified group of industry music professionals,
+          <br /> {/* forced line break after this sentence */}
+          including producers, directors, artists, performers, and
+          experienced event managers. Our shared mission is to provide
+          an unbiased platform for raw <br/> talents.
+        </p>
+      </div>
+      <button className="bg-orange-500 hover:bg-orange-600 text-white px-6 py-2 rounded-full font-semibold">
+        Connect
       </button>
     </div>
   );
