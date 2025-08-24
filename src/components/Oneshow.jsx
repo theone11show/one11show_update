df66e7015d8a0e9920c6e2726705e91d68d52d66
theone11show <team@theone11.show>
2025-08-24 12:54:49 +0530

    Update Oneshow.jsx

--- src/components/Oneshow.jsx
+++ src/components/Oneshow.jsx
@@ -41,7 +41,7 @@ const Oneshow = () => {
         <AboutSection />
 
         {/* Countdown Overlay (half on Hero, half on EventHeading) */}
-        <div className="absolute top-[29%] left-1/2 transform -translate-x-1/2 z-20 w-full max-w-5xl">
+        <div className="absolute top-[33%] left-1/2 transform -translate-x-1/2 z-20 w-full max-w-5xl">
           <CountdownPage />
         </div>
 
