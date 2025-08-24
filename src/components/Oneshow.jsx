89328bdc8f225e4d3e0f76f811a86a740d94e990
theone11show <team@theone11.show>
2025-08-24 12:37:51 +0530

    Update Oneshow.jsx

--- src/components/Oneshow.jsx
+++ src/components/Oneshow.jsx
@@ -3,16 +3,15 @@ import { useLocation } from "react-router-dom";
 import { scroller } from "react-scroll";
 import "./styles.css";
 import Layout from "./Animations/Layout";
-import AboutSection from "./AboutSection"; 
-import AboutUs from "./AboutUs";
-import CountdownPage from "./CountdownSection";
-import MusicBattle from "./MusicBattle";
+import AboutSection from "./AboutSection" ;
+import CountdownPage from "./CountdownSection"; 
 import EventHeading from "./EventHeading";
-import EventsSection from "./EventsCards"
+import EventsSection from "./EventsCards";
+import MusicBattle from "./MusicBattle";
+
 const Oneshow = () => {
   const location = useLocation();
 
-  // Handle auto-scroll from navigation
   useEffect(() => {
     if (location.state?.scrollToUpdates) {
       setTimeout(() => {
@@ -35,39 +34,22 @@ const Oneshow = () => {
     }
   }, [location]);
 
-  // Handle parallax side images
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop =
-        window.pageYOffset || document.documentElement.scrollTop;
-      const leftImage = document.querySelector(".side-image.left");
-      const rightImage = document.querySelector(".side-image.right");
-      if (leftImage)
-        leftImage.style.transform = `translateY(${scrollTop * 0.2}px)`;
-      if (rightImage)
-        rightImage.style.transform = `translateY(${scrollTop * 0.2}px)`;
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <Layout>
-      <div style={{ position:"relative" }}> >
+      <div className="relative">
+        {/* Hero Section */}
         <AboutSection />
-    
-  <div style={{ postion: "absolute",top: "50px", left: 0, right: 0, zIndex: 20 }}>
-  <CountdownPage />
-</div>
-      
-         {/*Heading*/}
-         <EventHeading/>
-         {/*Events Cars*/}
-         <EventsSection/>
-         {/*Music Battle*/}
-         <MusicBattle/>
 
-       
+        {/* Countdown Overlay (half on Hero, half on EventHeading) */}
+        <div className="absolute top-[29%] left-1/2 transform -translate-x-1/2 z-20 w-full max-w-5xl">
+          <CountdownPage />
+        </div>
+
+        {/* EventHeading (Countdown overlaps top area of this) */}
+        <EventHeading />
+
+        <EventsSection />
+        <MusicBattle />
       </div>
     </Layout>
   );
