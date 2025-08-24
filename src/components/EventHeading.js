3187f925d0d89c30541cb3c62216afbc5367c625
theone11show <team@theone11.show>
2025-08-24 18:10:55 +0530

    Update EventHeading.js

--- src/components/EventHeading.js
+++ src/components/EventHeading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const EventHeading = () => {
   return (
-    <div className="relative bg-gradient-to-r from-[#C90D16] to-[#520505] text-gray pl-28 pr-20 min-h-[1000px]">
+    <div className="relative  bg-gradient-to-b from-[#7a0000] to-[#1c0000] text-gray pl-28 pr-20 min-h-[1000px]">
       {/* Main Heading */}
       <h2 className="absolute top-[65%] text-6xl sm:text-7xl md:text-8xl font-extrabold uppercase tracking-wider drop-shadow-lg">
         UPCOMING ARTISTS
