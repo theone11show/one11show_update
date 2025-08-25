3d7259608ead622b5234c2f6460dcb14d1f247ca
theone11show <team@theone11.show>
2025-08-25 12:48:57 +0530

    Update MissionSection.js

--- src/components/MissionSection.js
+++ src/components/MissionSection.js
@@ -2,22 +2,19 @@ import React from "react";
 
 const MissionSection = () => {
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-200 text-center p-10">
+    <div className="flex flex-col items-center justify-center bg-gray-200 text-center px-4 sm:px-6 md:px-10 py-10">
       <div className="max-w-3xl"> 
-        <p className="mb-6 text-small text-gray-700 leading-relaxed">
+        <p className="mb-6 text-xs sm:text-sm md:text-base text-gray-700 leading-relaxed">
           We are a unified group of industry music professionals,
-          <br /> {/* forced line break after this sentence */}
+          <br />
           including producers, directors, artists, performers, and
           experienced event managers. Our shared mission is to provide
           an unbiased platform for raw <br/> talents.
         </p>
       </div>
-      <button
-  className="px-6 py-2 rounded-full font-semibold transition"
-  style={{ backgroundColor: "#fecd98", color: "black" }}
->
-  Connect
-</button>
+      <button className="bg-orange-500 hover:bg-orange-600 text-white px-3 py-1 rounded-full font-semibold text-[11px] sm:text-sm md:text-base">
+        Connect
+      </button>
     </div>
   );
 };
