5b809f408232024b293b975d76230b452242a1c8
theone11show <team@theone11.show>
2025-08-25 12:45:59 +0530

    Update CountdownSection.jsx

--- src/components/CountdownSection.jsx
+++ src/components/CountdownSection.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import CountdownTimer from "./CountdownTimer";
 import AboutSection from "./DescriptionSection";
 import EventBanner from "./EventBanner";
@@ -6,22 +5,36 @@ import MissionSection from "./MissionSection";
 
 const CountdownPage = () => {
   return (
-    <div className="w-full min-h-screen flex flex-col items-center justify-center px-4">
-      <div className="w-full max-w-4xl mx-auto">
-        {/* Top Section */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-0">
-          <CountdownTimer targetDate="2025-09-05T00:00:00" />
-          <AboutSection />
-        </div>
+    <div className="w-full min-h-screen flex items-center justify-center p-4 sm:p-6 md:p-8">
+      <div className="w-full max-w-7xl mx-auto">
+        {/* 2x2 Grid with wider right-side cards and equal heights */}
+        <div 
+          className="grid grid-rows-2 grid-cols-12 w-full h-[90vh]" 
+          style={{ gap: "0" }} // No gap
+        >
+          {/* Top Left - Countdown Timer */}
+          <div className="col-span-5 row-span-1 h-full">
+            <CountdownTimer />
+          </div>
 
-        {/* Bottom Section */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-0 mt-0">
-          <EventBanner />
-          <MissionSection />
+          {/* Top Right - About Section */}
+          <div className="col-span-7 row-span-1 h-full">
+            <AboutSection />
+          </div>
+
+          {/* Bottom Left - Event Banner */}
+          <div className="col-span-5 row-span-1 h-full">
+            <EventBanner />
+          </div>
+
+          {/* Bottom Right - Mission Section */}
+          <div className="col-span-7 row-span-1 h-full">
+            <MissionSection />
+          </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default CountdownPage;
