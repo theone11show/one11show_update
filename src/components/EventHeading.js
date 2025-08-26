692aa983553a576998807ada4aa2b88d03719036
theone11show <team@theone11.show>
2025-08-27 01:33:53 +0530

    Update EventHeading.js

--- src/components/EventHeading.js
+++ src/components/EventHeading.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-
 const EventHeading = () => {
   return (
-    <div className="relative  text-gray pl-6 sm:pl-16 md:pl-28 pr-6 sm:pr-12 md:pr-20 min-h-[900px]">
+    <div className="relative text-gray pl-6 sm:pl-16 md:pl-28 pr-6 sm:pr-12 md:pr-20 min-h-[900px]">
       {/* Main Heading */}
       <h2
         className="
           absolute  
           top-[30%] sm:top-[45%] md:top-[50%] lg:top-[55%] 
-          text-3xl sm:text-5xl md:text-7xl lg:text-8xl  
-          font-extrabold uppercase tracking-wider drop-shadow-lg
-          text-left
+          text-4xl sm:text-6xl md:text-[80px]   
+         uppercase tracking-wider drop-shadow-lg
+          text-left font-aton text-white
         "
       >
         UPCOMING ARTISTS
@@ -22,14 +21,13 @@ const EventHeading = () => {
         className="
           absolute 
           top-[42%] sm:top-[60%] md:top-[65%] lg:top-[70%] 
-          text-lg sm:text-2xl md:text-4xl lg:text-5xl 
+          text-lg sm:text-xl md:text-[23px] 
           text-gray-300 drop-shadow
-          text-left
+          text-left font-montserrat
         "
       >
         LIVE IN SILIGURI
       </p>
-  
     </div>
   );
 };
