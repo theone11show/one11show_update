4f6ed27a0b955423f09b281530ebab4e88a7cd37
theone11show <team@theone11.show>
2025-08-27 01:07:32 +0530

    Update EventsCards.js

--- src/components/EventsCards.js
+++ src/components/EventsCards.js
@@ -75,7 +75,7 @@ const EventsSection = () => {
   }, []);
 
   return (
-    <section className="py-12 px-6  relative">
+    <section className="pt-6 pb-12 px-6 relative">
       {/* Left Button (hide on small screens) */}
       {showLeft && (
         <button
