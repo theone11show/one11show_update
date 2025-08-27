fd8d41aab1c6dfba6039cba3930cc7e4b7a8eca9
theone11show <team@theone11.show>
2025-08-27 12:32:25 +0530

    Update UpcomingArtists.js

--- src/components/UpcomingArtists.js
+++ src/components/UpcomingArtists.js
@@ -3,7 +3,7 @@ import EventHeading from "./EventHeading";
 import EventsCards from "./EventsCards";
 const UpcomingArtists = () => 
   { return 
-    ( <section className="bg-gradient-to-r from-[#C90D16] to-[#1c0000] "> 
+    ( <section className="bg-gradient-to-r from-[#C90D16] to-[#1c0000]  relative"> 
 {/* Heading Section */}
   <EventHeading /> 
 {/* Events Cards */}
