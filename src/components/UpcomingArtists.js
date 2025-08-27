8af1f4ff9351887a21db74930e44e675542591fc
theone11show <team@theone11.show>
2025-08-27 12:20:31 +0530

    Update UpcomingArtists.js

--- src/components/UpcomingArtists.js
+++ src/components/UpcomingArtists.js
@@ -1,19 +1,14 @@
 import React from "react";
 import EventHeading from "./EventHeading";
 import EventsCards from "./EventsCards";
-
-const UpcomingArtists = () => {
-  return (
-    <section className="bg-gradient-to-r from-[#C90D16] to-[#1c0000] text-white px-4 sm:px-8 md:px-16 py-12 sm:py-16 md:py-24">
-      <div className="max-w-7xl mx-auto flex flex-col gap-12">
-        {/* Heading Section */}
-        <EventHeading />
-
-        {/* Events Cards */}
-        <EventsCards />
-      </div>
-    </section>
-  );
-};
-
+const UpcomingArtists = () => 
+  { return 
+    ( <section className="bg-gradient-to-r from-[#C90D16] to-[#1c0000] relative"> 
+{/* Heading Section */}
+  <EventHeading /> 
+{/* Events Cards */}
+  <EventsCards />
+  </section> ); 
+    };
 export default UpcomingArtists;
+                               
