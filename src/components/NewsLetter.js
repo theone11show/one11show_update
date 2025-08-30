d9e1e5c26fd8cec06ec06cd8412904de43a40e65
theone11show <team@theone11.show>
2025-08-30 18:25:02 +0530

    Update NewsLetter.js
    updated the font Family

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -70,7 +70,7 @@ const NewsLetter = () => {
               placeholder="Enter your email"
               required
               className="ml-4 bg-transparent border-none outline-none text-gray-600 
-                         text-[18px] font-montserrat tracking-wide leading-none flex-1"
+                         text-[18px] font-montserrat tracking-wide leading-none flex-1 font-[Poppins] placeholder:font-[Poppins]"
             />
 
             {/* Subscribe Button */}
