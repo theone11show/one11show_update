63f1d049addf797e331a0c86dceeab47fabd8523
theone11show <team@theone11.show>
2025-08-30 15:34:17 +0530

    Create Aboutus.js

---
+++ src/Pages/Aboutus.js
@@ -0,0 +1,54 @@
+import React from "react";
+import TeamDescription from "../components/TeamDescription";
+import NewsLetter from "../components/NewsLetter";
+
+const Aboutus = () => {
+  return (
+    <div>
+    <section
+   className="relative bg-conver bg-center bg-no-repeat min-h-screen flex flex-col justify-center pb-[60px]  overflow-x-hidden"
+  style={{ backgroundImage: "url('/aboutus.webp')" }}
+>
+  {/* Title */}
+  <h2 className=" md:-ml-[120px]  text-[40px] md:text-[56px] font-anton text-white text-center mt-[550px]">
+  About Us
+</h2>
+
+ {/* About description */}
+<div className="md:- mt-20 md:-ml-[20px] text-[14px] sm:text-[16px] md:text-[28px] text-[#ffffff] leading-relaxed text-center font-[Montserrat] relative z-10">
+  <span>
+    The One11 Show is an artist’s launchpad dedicated to discovering, mentoring, and launching authentic
+  </span>
+  <br />
+  <span>
+    musical and artistic talent. Powered by The Chordifiers Studio, we unite passionate artists and skilled
+  </span>
+  <br />
+  <span>
+    industry professionals committed to nurturing raw talent.
+  </span>
+  <br />
+  <span>
+    Through vibrant music battles, live sessions, and engaging podcasts, we provide artists with the
+  </span>
+  <br />
+  <span>
+    structure, mentorship, and opportunities they need to shine. We celebrate and uplift the vibrant creative
+  </span>
+  <br />
+  <span>
+    community around the world.
+  </span>
+</div>
+
+
+    </section>
+    <TeamDescription/>
+    <NewsLetter/>
+  </div>
+
+
+  );
+};
+
+export default Aboutus;
