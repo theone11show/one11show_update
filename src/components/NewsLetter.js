60f34f38d360edcd7bda1f4d86ba0452b891de6a
theone11show <team@theone11.show>
2025-08-30 19:51:27 +0530

    Update NewsLetter.js

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -70,10 +70,8 @@ const NewsLetter = () => {
               placeholder="Enter your email"
               required
               className="ml-4 bg-transparent border-none outline-none text-gray-600 
-           font-montserrat tracking-wide leading-none flex-1 font-[Poppins] placeholder:font-[Poppins]"
-style={{
-  fontSize: "clamp(14px, 3vw, 18px)",
-}}
+                         text-[18px] font-montserrat tracking-wide leading-none flex-1 font-[Poppins] placeholder:font-[Poppins]"
+            />
 
             {/* Subscribe Button */}
             <button
