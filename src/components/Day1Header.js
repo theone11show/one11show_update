7cdbd71219d117ca9ccf839ffcc7235eb2f7d58a
theone11show <team@theone11.show>
2025-08-31 16:31:11 +0530

    Update Day1Header.js

--- src/components/Day1Header.js
+++ src/components/Day1Header.js
@@ -5,21 +5,20 @@ const Day1Header = () => {
     <div className="flex flex-col justify-center items-start text-left px-6 sm:px-10 py-16 w-full md:w-1/2">
       {/* Title */}
       <h1
-        className="font-['Anton'] text-red-600 font-extrabold leading-none pt-40
-        text-5xl sm:text-7xl md:text-[168px] lg:text-[180px] whitespace-nowrap"
-      >
-        DAY 1
-      </h1>
+  className="font-['Anton'] text-red-600 font-extrabold leading-none pt-40
+  text-[96px] sm:text-[120px] md:text-[168px] lg:text-[180px] whitespace-nowrap"
+>
+  DAY 1
+</h1>
 
       {/* Date */}
       <p
-        className="mt-4 font-['Montserrat'] text-[#e4e2d2] 
-        whitespace-nowrap
-        text-xs sm:text-sm md:text-[22px]"
-      >
-        20 SEPTEMBER • SATURDAY
-      </p>
-
+  className="mt-4 font-['Montserrat'] text-[#e4e2d2] 
+  whitespace-nowrap
+  text-sm sm:text-base md:text-[22px]"
+>
+  20 SEPTEMBER • SATURDAY
+</p>
       {/* Buttons */}
       <div className="flex flex-nowrap gap-4 mt-8">
         <button
