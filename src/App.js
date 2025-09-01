1a2c2b6cea873d006bfd41bd7423305147d60b66
theone11show <team@theone11.show>
2025-09-01 12:54:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -57,6 +57,7 @@ import OurTeamComponents from './components/OurTeamComponents';
 import MusicBattleEvents from "./Pages/MusicBattleEvents";
 import Aboutus from "./Pages/Aboutus";
 import Day1 from "./Pages/Day1";
+import Day1 from "./Pages/Day2";
 
 
 
@@ -112,6 +113,7 @@ function App() {
         <Route path ='/Support' element ={<InfoSection/>}/>
         <Route path ='/eventSchedule' element ={<MusicBattleEvents/>}/> 
            <Route path ='/day1-events' element ={<Day1/>}/>
+           <Route path ='/day2-events' element ={<Day2/>}/>
 
 
 
