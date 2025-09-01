c885ddd5e0477ffc8f9e28f4c32f9bde8dbf69f3
theone11show <team@theone11.show>
2025-09-01 12:53:12 +0530

    Add files via upload
    Added Day 2 components

---
+++ src/components/Day2Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Day2Header = () => {
+  return (
+    <div className="flex flex-col justify-center items-start text-left px-6 sm:px-10 py-16 w-full md:w-1/2">
+      {/* Title */}
+      <h1
+  className="font-['Anton'] text-red-600 font-extrabold leading-none pt-40
+  text-[96px] sm:text-[120px] md:text-[168px] lg:text-[180px] whitespace-nowrap"
+>
+  DAY 2
+</h1>
+
+      {/* Date */}
+      <p
+  className="mt-4 font-['Montserrat'] text-[#e4e2d2] 
+  whitespace-nowrap
+  text-sm sm:text-base md:text-[22px]"
+>
+  21 SEPTEMBER • SUNDAY
+</p>
+      {/* Buttons */}
+      <div className="flex flex-nowrap gap-4 mt-8">
+        <button
+          className="font-['Poppins'] bg-white text-black rounded-full font-bold
+          px-3 py-1.5 sm:px-4 sm:py-2 md:px-5 md:py-2.5
+          min-w-[140px] md:min-w-[180px] lg:min-w-[200px]
+          text-xs sm:text-sm md:text-lg lg:text-xl
+          whitespace-nowrap
+          hover:bg-gray-200 transition"
+        >
+          BOOK TICKETS
+        </button>
+        <button
+          className="font-['Poppins'] bg-white text-black rounded-full font-bold
+          px-3 py-1.5 sm:px-4 sm:py-2 md:px-5 md:py-2.5
+          min-w-[140px] md:min-w-[160px] lg:min-w-[180px]
+          text-xs sm:text-sm md:text-lg lg:text-xl
+          whitespace-nowrap
+          hover:bg-gray-200 transition"
+        >
+          SEE LINEUP
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Day2Header;
---
+++ src/components/ExclusivePresence2.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+const ExclusivePresence2 = () => {
+  return (
+    <div
+      className="p-6 sm:p-8 bg-cover bg-center relative"
+      
+    >
+     
+      
+
+      {/* Content */}
+      <div className="relative z-10">
+        {/* Block 1 */}
+        <div className="mb-12 font-extrabold">
+          <h2 className="font-['Montserrat'] text-[#e4e2dd] text-[40px] mb-4">
+            PUJA-THEMED CULTURAL PERFORMANCE
+          </h2>
+          <p className="font-['Montserrat'] text-white text-[19px] leading-relaxed">
+            Day 2 brings a touch of tradition and celebration through specially 
+            curated cultural theme setting the mood for Durga Puja, offering 
+            a HeadStart for the puja festival.
+          </p>
+          
+        </div>
+
+        {/* Block 2 */}
+        <div className="mb-20 font-extrabold">
+          <h2 className="font-['Montserrat'] text-[#e4e2dd] text-[40px] mt-16">
+            CACTUSS
+          </h2>
+          <p className="font-['Montserrat'] text-white text-[19px] leading-relaxed">
+          Cactus is a Rock band from Kolkata, formed in 1992. 
+          The name was derived from the fact that the music scene was then 
+          devoid of life as in a desert and despite the odds, like a cactus,
+          they survived
+          </p>
+        </div>
+
+        {/* Block 3 */}
+        <div className="mt-16 mb-20 font-extrabold">
+          <h2 className="font-['Montserrat'] text-[#e4e2dd] text-[40px]">
+            FAKIRA
+          </h2>
+          <p className="font-['Montserrat'] text-white text-[19px] leading-relaxed">
+          Fakira is a renowned Bengali music band, known for blending 
+          soulful folk traditions with contemporary sounds. 
+          They are now coming to Siliguri to light up the evening 
+          with their mesmerizing live performance at The One11 Show.
+          </p>
+        </div>
+
+        {/* Block 4 */}
+        <div className="mt-16 mb-20">
+          <h2 className="font-['Montserrat'] text-[#e4e2dd] text-[40px] font-extrabold mt-16">
+            SURPRISE PERFORMANCES
+          </h2>
+          <p className="font-['Montserrat'] text-white text-[19px] leading-relaxed">
+          Semi-finalists from across the India will find themselves in this 
+          stage near you. From bands, singer-songwriters, Rappers, Musicians.
+           Find out whether they have what it takes to go to the finals.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExclusivePresence2;
