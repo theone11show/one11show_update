4be88bfa3b73e98972f48621ffa1e8dc68d5bd9c
theone11show <team@theone11.show>
2025-09-01 12:53:56 +0530

    Add files via upload
    Added Day2 page

---
+++ src/Pages/Day2.js
@@ -0,0 +1,140 @@
+import React from "react";
+import DayHeader from "../components/Day2Header";
+import ExclusivePresence2 from "../components/ExclusivePresence2";
+import GuestCards2 from "../components/GuestCards2";
+
+const DesktopLayout = () => {
+  return (
+    <div
+      className="hidden md:grid md:grid-cols-2 gap-12"
+      style={{
+        backgroundImage: "url('/day2bg.webp')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      {/* GuestCard on the left */}
+      <div className="order-1">
+        <GuestCards2 />
+      </div>
+
+      {/* Header + ExclusivePresence on the right */}
+      <div className="order-2">
+        <DayHeader />
+        <ExclusivePresence2 />
+      </div>
+    </div>
+  );
+};
+
+const MobileLayout = () => {
+  return (
+    <div
+      className="block md:hidden space-y-8 px-4 overflow-x-hidden"
+      style={{
+        backgroundImage: "url('/day2bg.webp')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <DayHeader />
+
+      {/* Guest 1 */}
+      <div>
+        <img
+          src="/day2.webp"
+          alt="day2"
+          loading="lazy"
+          className="rounded-xl h-80"
+        />
+        
+        <div className="mb-20 font-extrabold">
+          <h2 className="font-['Montserrat'] text-[#e4e2dd] text-[27px] mt-16">
+             PUJA-THEMED CULTURAL PERFORMANCE
+          </h2>
+          <p className="font-['Montserrat'] text-white text-[14px] leading-relaxed mt-4">
+             Day 2 brings a touch of tradition and celebration through specially 
+            curated cultural theme setting the mood for Durga Puja, offering 
+            a HeadStart for the puja festival.
+          </p>
+          
+        </div>
+      </div>
+
+      {/* Guest 2 */}
+      <div>
+        <img
+          src="/cactussday2.webp"
+          alt="/cactussday2"
+          loading="lazy"
+          className="rounded-xl h-80"
+        />
+        <div className="mb-20 font-extrabold">
+          <h2 className="font-['Montserrat'] text-[#e4e2dd] text-[27px] mt-16">
+             CACTUSS
+          </h2>
+          <p className="font-['Montserrat'] text-white text-[14px] leading-relaxed mt-4">
+          Cactus is a Rock band from Kolkata, formed in 1992. 
+          The name was derived from the fact that the music scene was then 
+          devoid of life as in a desert and despite the odds, like a cactus,
+          they survived
+          </p>
+        </div>
+      </div>
+
+      {/* Guest 3 */}
+      <div>
+        <img
+          src="/fakiraday2.webp"
+          loading="lazy"
+          alt="Fakira"
+          className="rounded-xl h-80"
+        />
+        <div className="mt-16 mb-20 font-extrabold">
+          <h2 className="font-['Montserrat'] text-[#e4e2dd] text-[27px]">
+         FAKIRA
+          </h2>
+          <p className="font-['Montserrat'] text-white text-[14px] leading-relaxed mt-4">
+           Fakira is a renowned Bengali music band, known for blending 
+          soulful folk traditions with contemporary sounds. 
+          They are now coming to Siliguri to light up the evening 
+          with their mesmerizing live performance at The One11 Show.
+        </p>
+        </div>
+      </div>
+
+      {/* Guest 4 */}
+      <div>
+        <img
+          src="/suprised.webp"
+          loading="lazy"
+          alt="suprised"
+          className="rounded-xl mt-4 h-80"
+        />
+        <div className="mt-16 mb-20">
+          <h2 className="font-['Montserrat'] text-[#e4e2dd] text-[27px] font-extrabold mt-16">
+             SURPRISE PERFORMANCES
+          </h2>
+          <p className="font-['Montserrat'] text-white text-[14px] leading-relaxed pb-5">
+           Semi-finalists from across the India will find themselves in this 
+          stage near you. From bands, singer-songwriters, Rappers, Musicians.
+           Find out whether they have what it takes to go to the finals.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const GuestsSection = () => {
+  return (
+    <div>
+      <DesktopLayout />
+      <MobileLayout />
+    </div>
+  );
+};
+
+export default GuestsSection;
