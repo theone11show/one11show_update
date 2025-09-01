179978cf578de4993b56598748c693a51297ba7f
theone11show <team@theone11.show>
2025-09-01 13:19:02 +0530

    Update ExclusivePresence.js

--- src/components/ExclusivePresence.js
+++ src/components/ExclusivePresence.js
@@ -16,11 +16,11 @@ const ExclusivePresence = () => {
           <h2 className="font-['Montserrat'] text-[#e4e2dd] text-[40px] mb-4">
             EXCLUSIVE PRESENCE
           </h2>
-          <p className="font-['Montserrat'] text-white text-[24px] leading-relaxed">
+          <p className="font-['Montserrat'] text-white text-[19px] leading-relaxed">
             Look who gets pranked next by RJ Praveen at the show, as he hosts
             the show and takes the event next level.
           </p>
-          <p className="font-['Montserrat'] text-white text-[24px] leading-relaxed mt-4">
+          <p className="font-['Montserrat'] text-white text-[19px] leading-relaxed mt-4">
             Talented dual-voice Shri Sairam Iyer will join as the judge for the
             semi-finals of the one1 music battle. See as he, along with other
             judges, selects the finalists for the music battle.
@@ -32,7 +32,7 @@ const ExclusivePresence = () => {
           <h2 className="font-['Montserrat'] text-[#e4e2dd] text-[40px] mt-16">
             Music battle series
           </h2>
-          <p className="font-['Montserrat'] text-white text-[24px] leading-relaxed">
+          <p className="font-['Montserrat'] text-white text-[19px] leading-relaxed">
             Semi-finalists from across India will find themselves on this stage
             near you. From bands, singer-songwriters, rappers, musicians—find
             out whether they have what it takes to go to the finals.
@@ -44,7 +44,7 @@ const ExclusivePresence = () => {
           <h2 className="font-['Montserrat'] text-[#e4e2dd] text-[40px]">
             Dj night with TASIA
           </h2>
-          <p className="font-['Montserrat'] text-white text-[24px] leading-relaxed">
+          <p className="font-['Montserrat'] text-white text-[19px] leading-relaxed">
             Electrifying DJ Night to end the day. Dj Tasia will take over the
             console, spinning dynamic beats that promise to uplift the crowd
             and keep the night alive.
@@ -56,7 +56,7 @@ const ExclusivePresence = () => {
           <h2 className="font-['Montserrat'] text-[#e4e2dd] text-[40px] font-extrabold mt-16">
             The Biggest Artist Meetup
           </h2>
-          <p className="font-['Montserrat'] text-white text-[24px] leading-relaxed">
+          <p className="font-['Montserrat'] text-white text-[19px] leading-relaxed">
             The first day will feature the biggest artist meetup in North Bengal,
             where artists of every genre, taste, and niche will come together
             to connect, bond, and network beyond boundaries of language and genre,
@@ -69,3 +69,4 @@ const ExclusivePresence = () => {
 };
 
 export default ExclusivePresence;
+
