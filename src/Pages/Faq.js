09d7a056f5294e3f9ba76d12695627377cea2306
theone11show <team@theone11.show>
2025-09-03 17:36:55 +0530

    Add files via upload

---
+++ src/Pages/Faq.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "../components/FAQ/Header.js";
+import Questions from "../components/FAQ/Questions";
+
+const Faq = () => {
+	return (
+		<div>
+		<Header/>
+		<Questions/>
+		</div>
+	)
+}
+
+export default Faq
